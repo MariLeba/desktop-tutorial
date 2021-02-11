f45d2bc88b43f193b108bb51dffa78d31f6772e4
MariLeba <72543134+MariLeba@users.noreply.github.com>
2021-02-11 22:12:12 +0200

    hi

---
+++ GuessMelody/GuessMelody/Assets/script/closePanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closePanel : MonoBehaviour
+{
+    public void closePanel()
+    {
+        destroy();
+    }
+}
